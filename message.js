8b8c5abe979656c40cbf7516ad36bece1f84e309
Red Man Real Estate LLC <redmanrealestate@outlook.com>
2024-08-27 15:57:40 -0500

    first push

--- message.js
+++ message.js
@@ -1,5 +1,15 @@
 class Message {
-   // Write code here!
+   constructor(name, commands){
+      this.name = name;
+      if (typeof name !== 'string') {
+         throw Error('Name must be a string');
+      }
+      this.commands = commands;
+      if (!Array.isArray(commands)) {
+         throw Error('Commands must be an array of Command objects');
+     }
+   
+ 
 }
-
+}
 module.exports = Message;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,22 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+it("throws error if a name is NOT passed into the constructor as the first parameter"), function(){
+    const output = new Message('test name');
+    expect(output).toBe("string");
 
+}
+it('constructor sets name', () => {
+    const message = new Message('Test message', []);
+    expect(message.name).toBe('Test message');
 });
+
+// it('contains a commands array passed into the constructor as the 2nd argument', () => {
+//     const command1 = new Command('MODE_CHANGE', 'LOW_POWER');
+//     const command2 = new Command('STATUS_CHECK');
+//     const commands = [command1, command2];
+//     const message = new Message('Test message with two commands', commands);
+
+//     expect(message.commands).toEqual(commands);
+// });
+})
